bfb4f0890b7c46b2c4e57293db3a5a239c34e874
admin <admin@adm.com>
2025-11-07 14:18:16 +0300

    cpu

--- app/transcription.py
+++ app/transcription.py
@@ -1,7 +1,6 @@
 import whisperx
-from typing import List, Dict
 from whisperx.diarize import DiarizationPipeline
-
+from typing import List, Dict
 
 def transcribe_audio(raw_bytes: bytes, lang: str) -> List[Dict]:
     file_path = "tmp.mp3"
@@ -9,23 +8,20 @@ def transcribe_audio(raw_bytes: bytes, lang: str) -> List[Dict]:
     with open(file_path, "wb") as f:
         f.write(raw_bytes)
 
-    # ✅ CPU-only
     device = "cpu"
-    compute_type = "int8_float32"   # ✅ лучший режим WhisperX для CPU
+    compute_type = "int8_float32"
 
-    print("Using device:", device, "| compute_type:", compute_type)
+    print("Using device:", device)
 
-    # ✅ Загружаем ASR модель (Whisper large-v2)
     model = whisperx.load_model(
         "base",
         device=device,
-        compute_type=compute_type,
+        compute_type=compute_type
     )
 
-    # ✅ транскрипция
     result = model.transcribe(file_path, language=lang)
 
-    # ✅ Диаризация — работает на CPU автоматически
+    # ✅ бесплатная модель сегментации вместо диаризации 3.1
     diarize_model = DiarizationPipeline(
         model_name="pyannote/segmentation",
         device=device,
@@ -34,17 +30,6 @@ def transcribe_audio(raw_bytes: bytes, lang: str) -> List[Dict]:
 
     diarization = diarize_model(file_path)
 
-    # ✅ объединяем слова и спикеров
     combined = whisperx.assign_word_speakers(diarization, result)
 
-    print(combined)
     return combined
-
-    # model = whisper.load_model("base")
-    #
-    #
-    # result = model.transcribe(file_path, language=lang)
-    # f.close()
-    # print(result)
-    # return result.get("text")
-
